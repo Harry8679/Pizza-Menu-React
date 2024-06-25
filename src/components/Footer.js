9d2f2fad24b613ca5e592a36298eb2dfe056b92d
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-25 02:35:07 +0200

    JavaScript Logic in Components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
 const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+//   if (hour >= openHour && hour <= closeHour) {
+//     alert('We\'re currently open');
+//   } else {
+//     alert('Sorry we\'re closed');
+//   }
   return (
     <footer>
       {new Date().toLocaleTimeString()} We're currently open
