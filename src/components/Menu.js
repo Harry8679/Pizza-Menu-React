ca3f033fef28c6eeee1ac434e56fae8c1eed65f3
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-24 15:09:46 +0100

    Loop on all pizzas from data.js file

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Pizza from './Pizza';
+import { pizzaData } from '../data';
 
 const Menu = () => {
   return (
     <main className='menu'>
       <h2>Our Menu</h2>
-      <Pizza name='Pizza Spinaci' ingredient='Tomato, mozarella, spinach, and ricotta cheese' photoName='pizzas/spinaci.jpg' price={10} />
+      {/* <Pizza name='Pizza Spinaci' ingredient='Tomato, mozarella, spinach, and ricotta cheese' photoName='pizzas/spinaci.jpg' price={10} /> */}
+      {pizzaData.map(pizza => (
+        <Pizza name={pizza.name} ingredient={pizza.ingredients} photoName={pizza.photoName} price={pizza.price} />
+      ))}
     </main>
   )
 }
