caf1dea9228169e5c64787fb3b42fcada3bc1bb3
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-25 11:47:10 +0100

    Footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  const hour = new Date().getHours();
-  const openHour = 12;
-  const closeHour = 22;
+  const hour = new Date().getHours(); // Récupère l'heure actuelle
+  const openHour = 12; // Heure d'ouverture
+  const closeHour = 22; // Heure de fermeture
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
-//   if (hour >= openHour && hour <= closeHour) {
-//     alert('We\'re currently open');
-//   } else {
-//     alert('Sorry we\'re closed');
-//   }
+
+  // Logs pour debug
+  console.log("Current hour:", hour);
+  console.log("Open hour:", openHour);
+  console.log("Close hour:", closeHour);
+  console.log("Is open:", isOpen);
+
   return (
-    <footer className='footer'>
-      {new Date().toLocaleTimeString()} We're currently open
+    <footer className="footer" style={{ color: "black", fontSize: "16px" }}>
+      <p>
+        {isOpen
+          ? `${new Date().toLocaleTimeString()} We're currently open`
+          : "Sorry, we're closed"}
+      </p>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
